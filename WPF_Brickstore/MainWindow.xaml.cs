bbc99abdb05d8155139d21c715516b2ba4dd26e6
DevDominik <71924117+DevDominik@users.noreply.github.com>
2024-09-12 09:15:22 +0200

    Update MainWindow.xaml.cs

--- WPF_Brickstore/MainWindow.xaml.cs
+++ WPF_Brickstore/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.Win32;
+using Microsoft.Win32;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,73 +22,104 @@ namespace WPF_Brickstore
     public partial class MainWindow : Window
     {
         static public ObservableCollection<LegoElem> legoElemek = new ObservableCollection<LegoElem>();
+        static public List<string> kategoriaElemek = new List<string>();
+        static public Dictionary<string, string> fajlLista = new Dictionary<string, string>();
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnNev_Click(object sender, RoutedEventArgs e)
-        {
-            string tbxSzoveg = tbxKereso.Text.Trim();
-            if (tbxSzoveg.Length < 1)
-            {
-                dgAdatok.ItemsSource = legoElemek;
-                return;
-            }
-            ObservableCollection<LegoElem> masolat = new ObservableCollection<LegoElem>();
-            foreach (LegoElem legoElem in legoElemek)
-            {
-                if (legoElem.Nev.Substring(0, tbxSzoveg.Length).ToLower() == tbxSzoveg.ToLower())
-                {
-                    masolat.Add(legoElem);
-                }
-            }
-            dgAdatok.ItemsSource = masolat;
-        }
-
-        private void btnId_Click(object sender, RoutedEventArgs e)
-        {
-            string tbxSzoveg = tbxKereso.Text.Trim();
-            if (tbxSzoveg.Length < 1)
-            {
-                dgAdatok.ItemsSource = legoElemek;
-                return;
-            }
-            ObservableCollection<LegoElem> masolat = new ObservableCollection<LegoElem>();
-            foreach (LegoElem legoElem in legoElemek)
-            {
-                if (legoElem.Id.Substring(0, tbxSzoveg.Length).ToLower() == tbxSzoveg.ToLower())
-                {
-                    masolat.Add(legoElem);
-                }
-            }
-            dgAdatok.ItemsSource = masolat;
-        }
-
         private void btnBetolt_Click(object sender, RoutedEventArgs e)
         {
-            dgAdatok.ItemsSource = null;
-            OpenFileDialog ofd = new OpenFileDialog();
+            OpenFolderDialog ofd = new OpenFolderDialog();
             ofd.InitialDirectory = "c:\\";
-            ofd.Filter = "BSX fájlok (*.bsx)|*.bsx";
             ofd.Multiselect = false;
             if (ofd.ShowDialog() != true)
             {
                 return;
             }
-            XDocument xaml = XDocument.Load(ofd.FileName);
-            legoElemek.Clear();
-            foreach (XElement item in xaml.Descendants("Item"))
+
+        }
+        private void Kereso()
+        {
+            ObservableCollection<LegoElem> masolat = new ObservableCollection<LegoElem>();
+            foreach (LegoElem legoElem in legoElemek)
             {
-                legoElemek.Add(new LegoElem(
-                    item.Element("ItemID").Value, 
-                    item.Element("ItemName").Value, 
-                    item.Element("CategoryName").Value,
-                    item.Element("ColorName").Value,
-                    Convert.ToUInt32(item.Element("Qty").Value)
-                    ));
+                bool megfelel = true;
+                if (tbxNevKereso.Text != null && tbxNevKereso.Text.Trim().Length > 0)
+                {
+                    if (legoElem.Nev.Substring(0, tbxNevKereso.Text.Trim().Length).ToLower() != tbxNevKereso.Text.Trim().ToLower())
+                    {
+                        megfelel = false;
+                    }
+                }
+                if (tbxIdKereso.Text != null && tbxIdKereso.Text.Trim().Length > 0)
+                {
+                    if (legoElem.Id.Substring(0, tbxIdKereso.Text.Trim().Length).ToLower() != tbxIdKereso.Text.Trim().ToLower())
+                    {
+                        megfelel = false;
+                    }
+                }
+                if (cboKategoriaKereso.SelectedItem.ToString() != "Nincs kiválasztva")
+                {
+                    if (legoElem.Kategoria != cboKategoriaKereso.SelectedItem.ToString())
+                    {
+                        megfelel = false;
+                    }
+                }
+                if (megfelel)
+                {
+                    masolat.Add(legoElem);
+                }
             }
-            dgAdatok.ItemsSource = legoElemek;
+            dgAdatok.ItemsSource = null;
+            dgAdatok.Items.Clear();
+            dgAdatok.ItemsSource = masolat;
+        }
+        private void tbxNevKereso_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Kereso();
+        }
+
+        private void tbxIdKereso_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Kereso();
+        }
+
+        private void cboKategoriaKereso_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Kereso();
+        }
+
+        private void lbxFajlok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            dgAdatok.ItemsSource = null;
+            if (lbxFajlok.SelectedItem != null)
+            {
+                XDocument xaml = XDocument.Load(fajlLista[lbxFajlok.SelectedItem.ToString()]);
+                legoElemek.Clear();
+                foreach (XElement item in xaml.Descendants("Item"))
+                {
+                    LegoElem legoElem = new LegoElem(
+                        item.Element("ItemID").Value,
+                        item.Element("ItemName").Value,
+                        item.Element("CategoryName").Value,
+                        item.Element("ColorName").Value,
+                        Convert.ToUInt32(item.Element("Qty").Value)
+                        );
+                    legoElemek.Add(legoElem);
+                    if (!kategoriaElemek.Contains(legoElem.Kategoria))
+                    {
+                        kategoriaElemek.Add(legoElem.Kategoria);
+                    }
+                }
+                kategoriaElemek.Sort();
+                kategoriaElemek.Insert(0, "Nincs kiválasztva");
+                cboKategoriaKereso.ItemsSource = kategoriaElemek;
+                cboKategoriaKereso.SelectedIndex = 0;
+                dgAdatok.ItemsSource = legoElemek;
+                Kereso();
+            }
         }
     }
 }
